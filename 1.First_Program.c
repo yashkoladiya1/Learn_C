c1de1b34ef3357dcee3df54df1dab9098dc07f33
yashkoladiya1 <koladiyayash104@gmail.com>
2026-07-22 07:52:09 +0200

    learn basic C concepts (printf, comments, stdio.h, return 0)

---
+++ 1.First_Program.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    // This is single line comment
+    printf("I Like Vadapav!\n"); 
+    printf("It's Really Good"); 
+    return 0;   
+    /*
+    This is Multi-Line comments
+
+    stdio.h its contain scanf, printf function
+
+    \n is used for new line character
+
+    return 0 mean this code runs successfully whereas any other digit is consider as not successfully
+
+    printf is used for print any statement
+
+    */
+}
